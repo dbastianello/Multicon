f25dbc29c7e77b763f78cef1c3e86aa9f71ac51b
Dan Bastianello <dbastianello@gmail.com>
2017-08-19 14:32:14 -0400

    Update multicon.py

--- multicon.py
+++ multicon.py
@@ -10,17 +10,17 @@
 #  full license information.
 #
 # =====================
-#  == MultiCon v0.02  	==
+#  == MultiCon v0.03  	==
 # =====================
 #
 # Author:     Daniele Bastianello
-# Version:    0.02
+# Version:    0.03
 # Description: This program's purpose it to provide the user the ability to decode/encode strings to and from
 #           various formats (such as shellcode, % or # delimited ASCII, ...). The utility is intended for SOC/NOC
 #           environments to help with packet breakdowns and what ever "Other" purposes the user can think of.
 #
 #              This tool is intended to be multi-platform eventually but has only been tested under Ubuntu 14.04, 
-#           14.10, 15.04 and Windows 8.1. The program is still under development thus problems may occur, these 
+#           14.10, 15.04 and Windows 8.1, 10 . The program is still under development thus problems may occur, these
 #           will eventually be corrected as my schedule allows.
 #           
 #           This program requires tkinter to function if under debian based system you need to install by issuing: 
@@ -30,6 +30,7 @@
 
 
 import platform
+import binascii
 from tkinter import *
 from tkinter import ttk
 
@@ -44,8 +45,8 @@ def mcon_lin():
     delimiter_menu.place(relx=0.9, rely=0.44, anchor=CENTER)
 
     open_button.place(relx=0.9, rely=0.35, anchor=CENTER)
-    decode_button.place(relx=0.9, rely=0.5, anchor=CENTER)
-    encode_button.place(relx=0.9, rely=0.55, anchor=CENTER)
+    encode_button.place(relx=0.9, rely=0.5, anchor=CENTER)
+    decode_button.place(relx=0.9, rely=0.55, anchor=CENTER)
     save_button.place(relx=0.9, rely=0.6, anchor=CENTER)
 
 
@@ -59,8 +60,8 @@ def mcon_win():
     delimiter_menu.place(relx=0.94, rely=0.44, anchor=CENTER)
 
     open_button.place(relx=0.94, rely=0.35, anchor=CENTER)
-    decode_button.place(relx=0.94, rely=0.5, anchor=CENTER)
-    encode_button.place(relx=0.94, rely=0.55, anchor=CENTER)
+    encode_button.place(relx=0.94, rely=0.5, anchor=CENTER)
+    decode_button.place(relx=0.94, rely=0.55, anchor=CENTER)
     save_button.place(relx=0.94, rely=0.6, anchor=CENTER)
 
 
@@ -83,7 +84,6 @@ def load_data(file_name):
 # original file.
 def save_file():
     from tkinter.filedialog import asksaveasfilename
-
     new_name = asksaveasfilename() + ".mcon"
     with open(new_name, 'w') as outFile:
         outFile.write(output_text.get(1.0, END))
@@ -95,26 +95,38 @@ def save_file():
 # The encoder currently only supports ASCII hex and UTF-8 and will eventually evolve to support UTF-16.
 # It also only deals with none, \x, % and # delimiters to detect the ASCII within the strings provided.
 def decode(data):
+    size = len(data) - 1
     output_text.delete(1.0, END)
-    check = delimiter_select.get()[0]
-    # below is needed since I do not want tmp_data to be a global variable
-    tmp_data = ""
+    decoded_text = ""
+    for idx, i in enumerate(data):
+        if i == "%" or "#":
+            tmp = ""
+            for j in range(0, 2, 1):
+                tmp += data[idx + j + 1]
+            decoded_text += chr(int(tmp, 16))
+        elif (i == "\x5c") and (data[idx + 1] == "u"):
+            tmp = ""
+            for j in range(0, 4, 1):
+                tmp += data[idx + j + 2]
+            decoded_text += chr(int(tmp, 16))
+        elif i == "0" and (data[idx+1] == "x"):
+            tmp = ""
+            for j in range(0, 4, 1):
+                tmp += data[idx + j + 1]
+            decoded_text += chr(int(tmp, 16))
+        elif (i == "u" or i == "U") and (data[idx + 1] == "+"):
+            tmp = ""
+            for j in range(0, 4, 1):
+                tmp += data[idx + j + 2]
+            decoded_text += chr(int(tmp, 16))
+        elif idx == size and decoded_text == "":
+            decoded_text = "Bad formatting, the input could not be interpreted."
 
-    if check is "\x5c":
-        for i in data:
-            if i is "\x5c":
-                tmp_data = tmp_data + "0"
-            else:
-                tmp_data = tmp_data + i
-        data = tmp_data
-        search_string = "0x([A-Fa-f0-9]{2})"
-    else:
-        search_string = delimiter_select.get() + "([A-Fa-f0-9]{2})"
-
-    decoded_text = re.sub(search_string, lambda mc: chr(int(mc.groups()[0], 16)), data)
-
-    if decoded_text is not None:
+    if decoded_text is not "":
         output_text.insert(INSERT, decoded_text)
+    else:
+        output_text.insert(INSERT,
+                           "\t\t   Bad input formatting\n\t\t\tASCII= %##,\n\t\tUNICODE= \x5cu#### U+#### u+####")
 
 
 # This definition has as input either, manually entered/pasted text in the input text box or the file
@@ -126,21 +138,37 @@ def encode():
     data = input_text.get(1.0, END)
     size = len(data) - 1
     output_text.delete(1.0, END)
-    encode_text = None
-
+    encode_text = ""
     if delimiter_select.get() == "none":
-        for i in range(0, size):
-            if i == 0:
-                encode_text = encode_text + format(ord(data[i]), "x")
-            encode_text = encode_text + " " + format(ord(data[i]), "x")
-    else:
-        for i in range(0, size):
-            encode_text = encode_text + delimiter_select.get() + format(ord(data[i]), "x")
+        encode_text = data
+    elif delimiter_select.get() == "%":
+        for idx, i in enumerate(data):
+            if idx < size:
+                encode_text = encode_text + "%" + format(ord(i), "x")
+    elif delimiter_select.get() == "0x":
+        for idx, i in enumerate(data):
+            if idx < size:
+                tmp = binascii.hexlify(i.encode('utf16'))[6:] + binascii.hexlify(i.encode('utf16'))[4:-2]
+                tmp = '{}'.format(tmp)[2:-1]
+                encode_text = encode_text + "0x" + tmp
+    elif delimiter_select.get() == "\x5cu":
+        for idx, i in enumerate(data):
+            if idx < size:
+                tmp = binascii.hexlify(i.encode('utf16'))[6:] + binascii.hexlify(i.encode('utf16'))[4:-2]
+                tmp = '{}'.format(tmp)[2:-1]
+                encode_text = encode_text + "\x5cu" + tmp
+    elif delimiter_select.get() == "u+" or "U+":
+        for idx, i in enumerate(data):
+            if idx < size:
+                tmp = binascii.hexlify(i.encode('utf16'))[6:] + binascii.hexlify(i.encode('utf16'))[4:-2]
+                tmp = '{}'.format(tmp)[2:-1]
+                encode_text = encode_text + "u+" + tmp
 
-    if encode_text is not None:
+    if encode_text is not "":
         output_text.insert(INSERT, encode_text)
     else:
         output_text.insert(INSERT, "\t\tDid you enter anything?\n\t\t         . .\n\t\t          v")
+
 
 # Below are the buttons function calls when clicked
 def onclick_open():
@@ -165,7 +193,7 @@ root = Tk()
 root.resizable(0, 0)
 root.title("MultiCon")
 
-main_paine = ttk.Frame(root, width=800, height=560)
+main_paine = ttk.Frame(root, width=800, height=600)
 main_paine.pack()
 
 input_label = ttk.Label(main_paine, text="Input Text")
@@ -175,7 +203,7 @@ output_text = Text(main_paine, width=85, height=15)
 
 delimiter_label = ttk.Label(main_paine, text="Delimiter")
 delimiter_select = StringVar(main_paine)
-delimiter_menu = ttk.OptionMenu(main_paine, delimiter_select, "none", "none", "%", "#", "\\x")
+delimiter_menu = ttk.OptionMenu(main_paine, delimiter_select, "none", "%", "0x", "\x5cu", "u+", "U+")
 
 open_button = ttk.Button(main_paine, text="Open", command=onclick_open)
 decode_button = ttk.Button(main_paine, text="Decode", command=onclick_decode)
